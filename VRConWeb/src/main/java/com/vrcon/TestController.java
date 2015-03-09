af04f17607600ae1f7ef081bfc7750234a4917bb
Hoon Kim <innamgo@naver.com>
2015-03-09 16:20:15 +0900

    1st

---
+++ VRConWeb/src/main/java/com/vrcon/TestController.java
@@ -0,0 +1,18 @@
+package com.vrcon;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/three/WebGL_Ani_Cloth")
+public class TestController {
+
+	@RequestMapping(value="html", method=RequestMethod.GET)
+	public String prepare(Model model) {
+		model.addAttribute("foo", "bar");
+		model.addAttribute("fruit", "apple");
+		return "/three/WebGL_Ani_Cloth";
+	}
+}
